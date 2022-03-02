7cd4b9aac755159ff0a2546f6aba0369a2f1d551
Noisysundae <Noisysundae@gmail.com>
2022-03-02 21:26:14 +0700

    Disable Logging Outside Unity Editor

--- TECHMANIA/Assets/Scripts/Components/Main Menu/Startup.cs
+++ TECHMANIA/Assets/Scripts/Components/Main Menu/Startup.cs
@@ -7,6 +7,9 @@ public class Startup : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+#if !UNITY_EDITOR
+        Debug.unityLogger.logEnabled = false;
+#endif
         Input.simulateMouseWithTouches = false;
         Paths.PrepareFolders();
         OptionsPanel.ApplyOptionsOnStartUp();
